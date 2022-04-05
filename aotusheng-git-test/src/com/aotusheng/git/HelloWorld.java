9f500d45f84a85c83eeb89215a0a5d25a9cefc9e
aotusheng <aotusheng41@gmail.com>
2022-04-05 20:41:02 +0800

    Master分支，新增代码-222

--- aotusheng-git-test/src/com/aotusheng/git/HelloWorld.java
+++ aotusheng-git-test/src/com/aotusheng/git/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
+
+        System.out.println("我是Master分支，新增代码222");
     }
 }
